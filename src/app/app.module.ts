7492ef6486c5689e9eda3c16ef4fb7c2420fdd9d
Sara Acuña <acunabenavidessaraelis@gmail.com>
2024-09-28 15:11:58 -0500

    First navbar, footer and app components

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { TaskListComponent } from './pages/components/components/task-list/task-list.component';
+import { ComboboxComponent } from './shared/components/combobox/combobox.component';
+import { CreateTaskComponent } from './pages/components/components/task-list/create-task/create-task.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    TaskListComponent,
+    ComboboxComponent,
+    CreateTaskComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
